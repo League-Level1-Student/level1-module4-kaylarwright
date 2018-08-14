489483bc816b7cba21b06b2a1e137d891751c949
kaylarwright <kayla.wright@gompersstudent.org>
2018-08-13 17:51:12 -0700

    Add files via upload

--- Car.java
+++ Car.java
@@ -1,4 +1,23 @@
 
-public class Car {
 
+
+public class Car {
+	String c = "color";
+	String t = "type";
+
+void setColor(String c) {
+	this.c = c;
+}
+void setType(String t) {
+	this.t = t;
+}
+String getColor() {
+	return this.c;
+}
+String getType() {
+	return this.t;
+}
+public String toString() {
+	return "You have a " + c + " " + t + " car";
+}
 }
--- CarRunner.java
+++ CarRunner.java
@@ -1,4 +1,20 @@
+package extra;
+
+import Car;
 
 public class CarRunner {
-
+public static void main(String[] args) {
+	Car w = new Car();
+	Car a = new Car();
+	Car r = new Car();
+w.setColor("white");
+a.setColor("red");
+r.setColor("blue");
+w.setType("Chevrolet");
+a.setType("Corvette");
+r.setType("Toyata");
+System.out.println(w.toString());
+System.out.println(a.toString());
+System.out.println(r.toString());
+}
 }
